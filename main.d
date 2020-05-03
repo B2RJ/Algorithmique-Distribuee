cc9f6b1a94d911741da69fc48eee470c790549c7
BRUNEAU RICHARD p1402059 <richard.bruneau@etu.univ-lyon1.fr>
2020-05-03 09:32:37 +0200

    setup project with grid

---
+++ main.d
@@ -0,0 +1,88 @@
+// import std.c.time;
+import std.stdio;
+import std.concurrency;
+import core.time;
+import std.algorithm;
+import std.math;
+
+struct CancelMessage{}
+
+struct Noeud
+{
+    Tid tid; //thread_ID
+    int lid; //logical_ID
+}
+
+void receiveAllFinalization(Noeud [][] childTid, int row, int col)
+{
+    for(int i=0 ; i<row ; ++i) {
+        for(int j=0 ; j<col ; ++j) {
+            receiveOnly!CancelMessage();
+        }
+    }
+}
+
+void spawnedFunc(int myId, int n)
+{
+  
+    Noeud upNeighbor, downNeighbor, leftNeighbor, rightNeighbor;
+
+    // waiting for the reception of information sent by the father
+    receive
+    (
+     (immutable(Noeud) uneighbor, immutable(Noeud) dneighbor, immutable(Noeud) lneighbor, immutable(Noeud) rneighbor)
+        {
+            upNeighbor = cast(Noeud)uneighbor;
+            downNeighbor = cast(Noeud)dneighbor;
+            leftNeighbor = cast(Noeud)lneighbor;
+            rightNeighbor = cast(Noeud)rneighbor;
+        }
+    );
+    
+    // Question 2
+    writeln("Je suis le processus ", myId, " et j'ai pour voisin du haut ", upNeighbor.lid, ", pour voisin du bas ", downNeighbor.lid, " pour voisin de gauche ", leftNeighbor.lid, " et pour voisin de droite ", rightNeighbor.lid);
+
+    // end of your code
+
+    send(ownerTid, CancelMessage());
+    
+}
+
+
+void main()
+{
+    // number of child processes (must be a number that can be sqrt)
+    int n = 25;
+    int row = 5;
+    int col = 5;
+
+    // spawn threads (child processes)
+    Noeud [][] childTid = new Noeud[][](row, col);
+    for(int i = 0; i < row; ++i) {
+        for(int j = 0; j < col; ++j) {
+            childTid[i][j].tid = spawn(&spawnedFunc, i*row + j, n);
+            childTid[i][j].lid = i*row + j;
+        }
+    }
+
+    for(int i=0 ; i<row ; ++i) {
+        for(int j=0 ; j<col ; ++j) {
+
+            Noeud nul;
+            nul.tid = Tid();
+            nul.lid = -1;
+
+            Noeud up, down, left, right;
+
+            up = i > 0 ? childTid[i-1][j] : nul;
+            down = i < row-1 ? childTid[i+1][j] : nul;
+            left = j > 0 ? childTid[i][j-1] : nul;
+            right = j < col-1 ? childTid[i][j+1] : nul;
+
+            send(childTid[i][j].tid, cast(immutable)up, cast(immutable)down, cast(immutable)left, cast(immutable)right);
+        }
+    }
+
+    // wait for all completions
+    receiveAllFinalization(childTid, row, col);
+}
