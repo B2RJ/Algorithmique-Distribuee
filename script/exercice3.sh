f063c6f28212b5c8415ac05897229b8429553c57
BRUNEAU RICHARD p1402059 <richard.bruneau@etu.univ-lyon1.fr>
2020-05-13 16:15:22 +0200

    Add a script

---
+++ script/exercice3.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+let "min = 1000000"
+let "max = 0"
+let "sum = 0"
+#dmd -of=./out/exercice2  ../exercice2.d
+for i in {1..1000}
+do
+    let current=$(./out/exercice3)
+    let "sum = sum + current"
+    if [ $min -gt $current ]
+    then    
+        let "min = current" 
+    fi
+    if [ $max -lt $current ]
+    then    
+        let "max = current" 
+    fi
+    echo $current
+done
+let "sum = sum / 1000"
+echo min : $min
+echo max : $max
+echo average = $sum
